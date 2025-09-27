038903a85cf783781208a1e08e31bf0628d42ee6
Billyow <jorgeandres14190@gmail.com>
2025-09-27 09:42:37 -0500

    Initial structure for the project

---
+++ src/main/java/com/billyow/app/boardang/BoardangApplication.java
@@ -0,0 +1,13 @@
+package com.billyow.app.boardang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billyow/app/boardang/BoardangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billyow.app.boardang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
