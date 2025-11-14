1e6184cbbae1cc6d527cdef33380185e2548167b
Billyow <jorgeandres14190@gmail.com>
2025-11-14 09:31:37 -0500

    rename the column repository for consistency

--- src/main/java/com/billyow/app/boardang/boardColumn/repository/IBoardColumnRepository.java
+++ src/main/java/com/billyow/app/boardang/boardColumn/repository/IBoardColumnRepository.java
@@ -2,9 +2,9 @@ package com.billyow.app.boardang.boardColumn.repository;
 import com.billyow.app.boardang.boardColumn.model.BoardColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 @Repository
-public interface IBoardColumn extends JpaRepository<BoardColumn, Long> {
+public interface IBoardColumnRepository extends JpaRepository<BoardColumn, Long> {
      List<BoardColumn> findByBoard_IdOrderByPositionAsc(Long boardId);
+     void deleteByBoard_Id(Long boardId);
 }
