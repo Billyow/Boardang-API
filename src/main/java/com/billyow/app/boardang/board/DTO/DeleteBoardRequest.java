c0ec1e53b93806fa19815ece750631a2d00b8426
Billyow <jorgeandres14190@gmail.com>
2025-11-14 17:20:17 -0500

    delete the deleteboardrequest so the request only has the userId

--- src/main/java/com/billyow/app/boardang/board/DTO/DeleteBoardRequest.java
+++
@@ -1,8 +0,0 @@
-package com.billyow.app.boardang.board.DTO;
-
-import com.billyow.app.boardang.user.DTO.SimpleUserDTO;
-
-public record DeleteBoardRequest(
-        Long boardId,
-        Long userId) {
-}
